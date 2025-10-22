a861994d65f1e04fce6f8528b8e8b3fe5c53474b
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2025-10-22 20:12:03 +0000

    Playita

--- sketch.js
+++ sketch.js
@@ -1,14 +1,51 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
-  createCanvas(800,800)
+  createCanvas(600, 400);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+
+  background(135, 206, 235); 
+
+
+  fill(255, 223, 0);
+  noStroke();
+  ellipse(500, 80, 100, 100);
+
+
+  fill(0, 191, 255);
+  rect(0, 200, width, 120);
+
+
+  fill(173, 216, 230);
+  for (let x = 0; x < width; x += 40) {
+    ellipse(x, 200, 80, 20);
+  }
+
+
+  fill(238, 214, 175);
+  rect(0, 320, width, 80);
+
+  stroke(139, 69, 19);
+  strokeWeight(10);
+  line(100, 320, 100, 220);
+
+  noStroke();
+  fill(34, 139, 34);
+
+  triangle(100, 220, 60, 180, 120, 200);
+  triangle(100, 220, 140, 180, 80, 200);
+  triangle(100, 220, 70, 200, 130, 210);
+  triangle(100, 220, 110, 180, 150, 210);
+
+
+  fill(255, 69, 0);
+  triangle(400, 300, 450, 300, 425, 250);
+  stroke(160, 82, 45);
+  strokeWeight(4);
+  line(425, 300, 425, 330);
+
+  noStroke();
+  fill(255, 105, 180);
+  rect(360, 340, 120, 30, 5);
 }
